e4281b6e6ed846edcce06bd238d061ac8678bac4
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-04-07 07:47:32 +0800

    Imports

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,7 +1,41 @@
 // Declaration imports
 
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
 
 const Template : React.FC = () => {
+
+    return (
+
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+
+                    <IonButtons slot="start">
+
+                        <IonMenuButton></IonMenuButton>
+                    </IonButtons> 
+
+                    <IonTitle>
+                        Template
+
+                    </IonTitle>
+
+
+                  
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen> 
+
+                <h1>Template</h1>
+            </IonContent>
+
+
+        </IonPage>
+
+
+    );
 
 
 };
