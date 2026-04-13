ecd5b54b901bebc006575443b60dc7174677fa1f
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-04-14 07:43:28 +0800

    AAdded tabs files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,41 @@
+import { IonTabBar, IonTabs, IonTitle, IonToolbar } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+
 const Home: React.FC = () => {
 
   return(
 
+    <IonReactRouter>
 
-    
+      <IonTabs>
+
+        <IonTabBar slot="bottom">
+
+          <IonToolbar>
+
+            <IonTitle>
+
+              Tabs
+
+
+            </IonTitle>
+
+
+          </IonToolbar>
+          {/**/}
+
+
+
+        </IonTabBar>
+
+        IonRouter
+
+
+      </IonTabs>
+    </IonReactRouter>
+
+
+
   );
 
 
