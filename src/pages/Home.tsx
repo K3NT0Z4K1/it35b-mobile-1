b1a21dca8cc2b5a6ccaa095b6e0aaf83e9bb3c9e
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-04-14 07:49:06 +0800

    Updated Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import { IonTabBar, IonTabs, IonTitle, IonToolbar } from "@ionic/react";
+import { IonRouterOutlet, IonTabBar, IonTabs, IonTitle, IonToolbar } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
+import { Route } from "react-router";
+import Feed from "./home-tabs/Feed";
 
 const Home: React.FC = () => {
+
+
+  const tabs = [
+
+
+    {name: 'Feed', tab: 'feed', url: '/app/home/feed, icon:bookOutline'},
+    {name: 'Favorites', tab: 'favorites', url: '/app/home/favorites, icon:star'},
+    {name: 'Search', tab: 'serach', url: '/app/home/search, icon:search'},
+  ]
 
   return(
 
@@ -28,7 +39,13 @@ const Home: React.FC = () => {
 
         </IonTabBar>
 
-        IonRouter
+        <IonRouterOutlet>
+
+          <Route exact path = "app/home/feed" component= {Feed} />
+          <Route exact path = "app/home" />
+
+
+        </IonRouterOutlet>
 
 
       </IonTabs>
