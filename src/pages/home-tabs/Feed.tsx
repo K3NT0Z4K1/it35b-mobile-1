9216f82d62e6367bb53d26569f94f8aa2e9dd988
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-04-21 08:18:58 +0800

    Added for fixing later

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,17 +1,40 @@
 // Declaration imports
 
 import { 
+    IonButton,
     IonButtons, 
+    IonCard, 
+    IonCardContent, 
+    IonCardHeader, 
+    IonCardSubtitle, 
+    IonCardTitle, 
+    IonCol, 
     IonContent, 
+    IonGrid, 
     IonHeader, 
+    IonIcon, 
+    IonItem, 
+    IonLabel, 
+    IonList, 
     IonMenuButton, 
     IonPage, 
+    IonRow, 
     IonTitle, 
     IonToolbar } 
 from "@ionic/react";
+import { chatboxOutline, shareOutline, thumbsDownOutline } from "ionicons/icons";
 
 
 const Feed : React.FC = () => {
+
+    const games = [
+
+        {name: 'Pokemon Yellow'},
+        {name: 'Mega Man X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+        {name: 'Super Mario World'}
+]
 
     return (
 
@@ -24,20 +47,60 @@ const Feed : React.FC = () => {
                         <IonMenuButton></IonMenuButton>
                     </IonButtons> 
 
-                    <IonTitle>
-                        Template
-
-                    </IonTitle>
-
-
-                  
-                </IonToolbar>
+                    <IonTitle>Feed</IonTitle>
+                    </IonToolbar>
             </IonHeader>
 
-            <IonContent fullscreen> 
+            <IonContent className="ion-padding"> 
 
-                <h1>Template</h1>
+                <IonList>
+
+                    {games.map((item,index) => (
+
+                       <IonCard>
+                    <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                    <IonCardHeader>
+                        <IonCardTitle>Card Title</IonCardTitle>
+                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                    </IonCardHeader>
+
+                    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol>
+                                <IonButton fill="clear" expand="full">
+                                    <IonIcon icon={thumbsDownOutline}></IonIcon>
+                                    <IonLabel style={{marginLeft: '5px'}}>Like</IonLabel>
+
+                                </IonButton>
+                            </IonCol>
+                            <IonCol>
+                                <IonButton fill="clear" expand="full">
+                                    <IonIcon icon={chatboxOutline}></IonIcon>
+                                    <IonLabel style={{marginLeft: '5px'}}>Comment</IonLabel>
+
+                                </IonButton>
+                            </IonCol>
+                            <IonCol>
+                                <IonButton fill="clear" expand="full">
+                                    <IonIcon icon={shareOutline}></IonIcon>
+                                    <IonLabel style={{marginLeft: '5px'}}>Share</IonLabel>
+
+                                </IonButton>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonCard>
+                
+
+              
+                    ))}
+                </IonList>
             </IonContent>
+
+
+              
 
 
         </IonPage>
